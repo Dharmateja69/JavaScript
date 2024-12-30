26c2c23dae8ddc69aae7d5f6eeae8a683da15e8e
Dharmateja69 <ddharmateja69@gmail.com>
2024-12-30 22:39:13 +0530

    number

--- Basics/app.js
+++ Basics/app.js
@@ -1,9 +1,37 @@
-//if we declare a variable with const then its value cannot be changed ones it is assigned
-//
-// const name = "sudhu"
-// name = "shdu"
-// console.log(name)
-// app.js:4 Uncaught TypeError: Assignment to constant variable.
-// at app.js:4:6
+//primitive data type
+let num = 10.01;
+console.log(typeof num)
+// Basic Math
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2 * 2)
+console.log(2 + 3)
+console.log(2 % 2)
+console.log(2 / 2)
+console.log(2 ** 2)//power of variable 2^2 =4
+console.log("===========================================")
 
-//Assignment
+let counter = 0;
+counter++;
+counter++;
+counter++;
+counter++;
+counter++;
+counter++;
+counter++;
+counter--;
+counter--;
+counter--;
+counter--;
+counter--;
+counter--;
+counter--;
+
+
+
+
+console.log(counter)
+
+
+
+
