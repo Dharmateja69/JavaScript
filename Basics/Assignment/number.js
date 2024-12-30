160b6d8ceb72c0b74718b1ecbd8bf55685489d01
Dharmateja69 <ddharmateja69@gmail.com>
2024-12-30 22:43:30 +0530

    n2ass

---
+++ Basics/Assignment/number.js
@@ -0,0 +1,9 @@
+let firstfav = 6;
+let secondfav = 15
+console.log(firstfav + secondfav)
+console.log(firstfav - secondfav)
+console.log(firstfav * secondfav)
+console.log(firstfav / secondfav)
+console.log(firstfav % secondfav)
+console.log(firstfav ** secondfav)
+
--- Basics/app.js
+++ Basics/app.js
@@ -1,36 +1,36 @@
-//primitive data type
-let num = 10.01;
-console.log(typeof num)
-// Basic Math
-console.log(2 + 2)
-console.log(2 - 2)
-console.log(2 * 2)
-console.log(2 + 3)
-console.log(2 % 2)
-console.log(2 / 2)
-console.log(2 ** 2)//power of variable 2^2 =4
-console.log("===========================================")
+// //primitive data type
+// let num = 10.01;
+// console.log(typeof num)
+// // Basic Math
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 + 3)
+// console.log(2 % 2)
+// console.log(2 / 2)
+// console.log(2 ** 2)//power of variable 2^2 =4
+// console.log("===========================================")
 
-let counter = 0;
-counter++;
-counter++;
-counter++;
-counter++;
-counter++;
-counter++;
-counter++;
-counter--;
-counter--;
-counter--;
-counter--;
-counter--;
-counter--;
-counter--;
-
-
+// let counter = 0;
+// counter++;
+// counter++;
+// counter++;
+// counter++;
+// counter++;
+// counter++;
+// counter++;
+// counter--;
+// counter--;
+// counter--;
+// counter--;
+// counter--;
+// counter--;
+// counter--;
 
 
-console.log(counter)
+
+
+// console.log(counter)
 
 
 
