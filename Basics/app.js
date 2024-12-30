b26aa9df47f09e7df1028ff70a56b2f626f5cbb7
Dharmateja69 <ddharmateja69@gmail.com>
2024-12-30 22:19:56 +0530

    variable

--- Basics/app.js
+++ Basics/app.js
@@ -1,7 +1,17 @@
-console.log("heloo sidhu")
-alert("helo my name is sidhu")
-console.table({ name: "huxn", age: 20 })
-console.clear()
-console.table({ name: "huxn", age: 20 })
+// hjdfhvkdhdkbhd comments the line  we use ctrl+/ where after the running the code it will not compile the the commentscode
+//shortcut for conole.log( clg),(log)
 
 
+// variables ->a quantity which can be changed during the operation in the code
+//(reserved keyword)- (variable name)- (type of value) 
+
+// let name; // declaring a variable
+// console.log(name)
+
+// let name = 5; // Assigning  a value
+// let name1 = "sidhu"
+// console.log(name)
+// console.log(name1)
+// console.log({ name, name1 })
+
+//Conventation used to create a variable we use let keyword
