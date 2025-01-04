b46eb7829ea648d21e00edce57e74fbf09d6b07a
Dharmateja69 <ddharmateja69@gmail.com>
2025-01-04 22:57:29 +0530

    ArrowFunctions

---
+++ Basics/ArrowFunction.js
@@ -0,0 +1,32 @@
+// //Arrow functions, also known as fat arrow functions, are a concise and shorter way to define functions in javaScript. They were introduced in ECMAScript 6 (ES6) and provide a more compact syntax comapred to traditional function expressions.
+
+
+// function greet(username) {
+//     return `Hello ${username}`;
+// }
+
+// console.log(greet("sidhu"))
+
+// // useing arrow function
+
+// greet = (username) => {
+//     return `hellow ${username}`
+// }
+
+// console.log(greet("sidhu"))
+
+// // cleaned arrow functions for single parameter
+
+// greet = username => `Hello ${username}`
+// console.log(greet("dharma"))
+
+// // const function
+
+// function double(number) {
+//     return number * 2
+// }
+// console.log(double(5))
+// // refactoring the code using arrow function
+
+// const doubl = number => number * 2
+// console.log(doubl(4))
--- Basics/Assignment/number.js
+++ Basics/Assignment/number.js
@@ -1,9 +1,83 @@
 let firstfav = 6;
 let secondfav = 15
-console.log(firstfav + secondfav)
-console.log(firstfav - secondfav)
-console.log(firstfav * secondfav)
-console.log(firstfav / secondfav)
-console.log(firstfav % secondfav)
-console.log(firstfav ** secondfav)
+// console.log(firstfav + secondfav)
+// console.log(firstfav - secondfav)
+// console.log(firstfav * secondfav)
+// console.log(firstfav / secondfav)
+// console.log(firstfav % secondfav)
+// console.log(firstfav ** secondfav)
+
+if (firstfav % 2 == 0) {
+    console.log("This is even")
+}
+else {
+    console.log("This is odd")
+}
+
+const flop = 3.5059343999202
+console.log(Math.floor(flop))
+
+const fact = 5
+let num = 1
+for (let index = 2; index <= fact; index++) {
+    num = num * index
+
+}
+console.log(num)
+
+for (let index = 1; index < 100; index++) {
+    console.log(Math.random())
+
+}
+
+//gcd
+let first = 36
+let second = 60
+while (first != second) {
+    if (first > second) {
+        first = first - second
+    }
+    else {
+        second = second - first
+    }
+}
+console.log(first)
+
+// hard 
+//sidhu
+
+const sidhu = 2
+
+let count = 0
+for (let index = 2; index <= sidhu; index++) {
+    if (sidhu % index == 0) {
+        count++;
+    }
+
+}
+if (count > 1) {
+    console.log("not sidhu")
+} else {
+    console.log("sidhu")
+}
+
+
+
+
+for (let sidhu = 1; sidhu <= 100; sidhu++) {
+    let count1 = 0
+
+    for (let index = 1; index <= 100 / 2; index++) {
+        if (sidhu % index == 0) {
+            count1++;
+        }
+
+    }
+    if (count1 > 1) {
+        console.log(`not prime ${sidhu}`)
+    } else {
+        console.log(`prime ${sidhu}`)
+    }
+
+}
 
