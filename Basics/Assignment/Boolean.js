8126467f4c7d049db9dc00e6b83a5796cb7cfea4
Dharmateja69 <ddharmateja69@gmail.com>
2025-01-07 16:16:54 +0530

    boolean

--- Basics/Assignment/Boolean.js
+++ Basics/Assignment/Boolean.js
@@ -65,3 +65,53 @@
 // } else {
 //     console.log("number is not greater then 10")
 // }\
+
+// const storedname = "sidhu"
+// const password = "123*&123"
+
+// const username = "sidhu"
+// const key = "123*123"
+
+// const agreuser = (storedname === username)
+// const keyuseer = (password === key)
+
+// if (agreuser && keyuseer) {
+//     console.log("succesfuly logined...")
+// }
+// else {
+//     console.log("unsuccessfuly retry again...")
+// }
+
+// if (storedname !== username) {
+//     console.log("Give the valid or correct uername...")
+// }
+// if (password !== key) {
+//     console.log("Give the valid or correct password...")
+
+// }
+// let light = true
+// if (!light) {
+//     console.log("on")
+// } else {
+//     console.log("off")
+// }
+
+const stri = "1221"
+let i = 0
+let flag = 1
+let j = stri.length - 1
+while (i <= j) {
+    if (stri[i] != stri[j]) {
+        flag = 0
+        break;
+    }
+    i++
+    j--
+
+}
+if (flag) {
+    console.log("it is a palindrome...")
+}
+else {
+    console.log("it is not a palindrome...")
+}
--- Basics/Destructing.js
+++ Basics/Destructing.js
@@ -1,2 +1,3 @@
 // Destructing allows us to "unpack " values from data-structures (arrays, objects) into separate distinct variables.
 // 5:30:38
+
