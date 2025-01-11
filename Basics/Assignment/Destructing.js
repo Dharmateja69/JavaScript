1c3f4df5ef367ea659adbe88d6eee4a1a0ae87f8
Dharmateja69 <ddharmateja69@gmail.com>
2025-01-11 18:49:11 +0530

    destructing

---
+++ Basics/Assignment/Destructing.js
@@ -0,0 +1,7 @@
+const colors = ["red", "green", "blue", "yellow", "orange"]
+
+//your task is to use array destructing to extract the first three colors from the colors array and assign them to separate vaiable named color1, color2, color3.
+// After extracting the colors, log each variables value to the console.
+
+const [color1, color2, color3] = colors
+console.log(color1, color2, color3)
--- Basics/Destructing.js
+++ Basics/Destructing.js
@@ -1,3 +1,37 @@
 // Destructing allows us to "unpack " values from data-structures (arrays, objects) into separate distinct variables.
 // 5:30:38
 
+// const foo = ["one", "two", "three"]
+
+// const [A, B, C] = foo
+
+// console.log(A, B, C)
+
+
+// function f() {
+//     return [1, 2]
+// }
+// let a, b
+
+// [a, b] = f()
+// console.log(a)
+// console.log(b)
+
+// //how to ignore a funciton returning
+
+// function f() {
+//     return [1, 2, 3]
+// }
+// const [V, , D] = f()
+// console.log(V)
+// console.log(D)
+
+//Assigning the rest of an array to a varaible
+const [e, ...h] = ["one", "two", "three", "true", "12", ["one"]]
+
+console.log(e)
+console.log(h)
+
+// In object destructing order doesnt matter but the name does matter
+
+// 5:44:58
