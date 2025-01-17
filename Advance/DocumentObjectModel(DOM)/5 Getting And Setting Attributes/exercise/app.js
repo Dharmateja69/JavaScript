34031827461ec0b370ce40ffc8ccc9da50946f28
Dharmateja69 <ddharmateja69@gmail.com>
2025-01-17 21:53:05 +0530

    attributes>

--- Advance/DocumentObjectModel(DOM)/5 Getting And Setting Attributes/exercise/app.js
+++ Advance/DocumentObjectModel(DOM)/5 Getting And Setting Attributes/exercise/app.js
@@ -2,3 +2,14 @@
 // 2. Use getAttribute(attrName) to check the attribute.
 // 3. Select a-two & set "href" attribute
 // setAttribute(attrName, value)
+
+
+const ancor = document.querySelector('a')
+console.log(ancor.href)
+console.log(ancor.getAttribute("href"))
+
+const second = document.querySelector(".a-two")
+console.log(second.href = "www.school.com")
+const area = document.querySelector("input")
+area.setAttribute("placeholder", "hi this new code")
+
--- Advance/DocumentObjectModel(DOM)/5 Getting And Setting Attributes/live coding/app.js
+++ Advance/DocumentObjectModel(DOM)/5 Getting And Setting Attributes/live coding/app.js
@@ -1,5 +1,41 @@
-// href
-// value
-// type
-// getAttribute(attrName)
-// setAttribute(attrName, value)
+// // href
+// // value
+// // type
+// // getAttribute(attrName)
+// // setAttribute(attrName, value)
+
+// // console.log(document.querySelector('a'))
+// // console.log(document.querySelector('input'))
+
+// const link = document.querySelector('a')
+// const lin = document.querySelector('input')
+// // console.log(link.href)
+// // console.log(lin.value)
+// // console.log(lin.type)
+
+// // console.log(link.href = "www.sidhu.com")//used to change the existing value of the 
+
+// // //setting attribute
+// // link.href = "www.sidhu.com"
+// // console.log((lin.value = "name"))
+// // console.log((lin.type = "password"))
+// // console.log((lin.placeholder = "enter the data"))
+
+// // getAttribute(attrName)
+
+// // console.log(lin.getAttribute("type"))
+// // console.log(lin.getAttribute("placeholder"))
+// // console.log(lin.getAttribute("value"))//this also
+
+// //setAttribute(attrname,value)
+// lin.setAttribute("value", "hi")//this cannot be changed 
+// lin.setAttribute("placeholder", "hi bro")
+// lin.setAttribute("type", "password")
+
+
+
+
+
+
+
+
