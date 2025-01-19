1340dc54e324cf24caa4f69930d3473611047984
Dharmateja69 <ddharmateja69@gmail.com>
2025-01-19 19:32:33 +0530

    Project-1 Random Emojis

--- Advance/DocumentObjectModel(DOM)/Projects/1. Random Emojies/app.js
+++ Advance/DocumentObjectModel(DOM)/Projects/1. Random Emojies/app.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector("#emoji");
+const btn = document.querySelector("#emoji");//for id we will use this. # type. for class we will use (.) dot operator
 
 const emojis = [
   "😆",
@@ -38,5 +38,6 @@ const emojis = [
 ];
 
 btn.addEventListener("mouseover", () => {
-  btn.innerText = emojis[Math.floor(Math.random() * emojis.length)];
-});
+  btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)]
+  // console.log(Math.floor(Math.random() * emojis.length))//we use this because the randome //value will not exceed from this range
+})
