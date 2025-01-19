6665e78ce49cda6259cfbeffa7c5a293c91a3f76
Dharmateja69 <ddharmateja69@gmail.com>
2025-01-19 19:08:51 +0530

    mouse,keyboard> Events

--- Advance/DocumentObjectModel(DOM)/10. Events In-Depth/1. Mouse/app.js
+++ Advance/DocumentObjectModel(DOM)/10. Events In-Depth/1. Mouse/app.js
@@ -21,3 +21,57 @@ let seven = document.querySelector(".mousemove");
 let eight = document.querySelector(".mouseout");
 let nine = document.querySelector(".mouseover");
 let ten = document.querySelector(".mouseup");
+// 9.34.51
+
+one.addEventListener("click", () => {
+    one.classList.toggle("red"); //on left click the color will change.
+});
+
+two.addEventListener("contextmenu", () => {
+    two.classList.toggle("blue"); //on right click the colour will change.
+});
+
+three.addEventListener("dblclick", () => {
+    three.classList.toggle("orange");
+});
+
+four.addEventListener("mousedown", () => {
+    four.classList.toggle("steelblue")
+})
+
+
+five.addEventListener("mouseenter", () => {
+    five.style.color = "red"
+    console.log("five")
+})
+six.addEventListener("mouseleave", () => {
+    six.style.color = "yellow"
+    console.log("six")
+})
+
+seven.addEventListener("mousemove", () => {
+    seven.textContent = "Hello!!"
+})
+
+
+seven.addEventListener("mousemove", () => {
+    seven.textContent = "Hello!! 7"
+
+})
+
+
+eight.addEventListener("mouseout", () => {
+    eight.textContent = "Hello!! 8"
+
+})
+
+
+nine.addEventListener("mouseover", () => {
+    nine.textContent = "Hello!! 9"
+
+})
+
+ten.addEventListener("mouseup", () => {
+    ten.textContent = "Hello!! 10"
+
+})
--- Advance/DocumentObjectModel(DOM)/10. Events In-Depth/2. Keyboard/app.js
+++ Advance/DocumentObjectModel(DOM)/10. Events In-Depth/2. Keyboard/app.js
@@ -1,2 +1,19 @@
 // https://www.w3schools.com/jsref/obj_keyboardevent.asp
 const input = document.querySelector("input");
+
+// input.addEventListener("keypress", () => {
+//     console.log("key press!!")
+// })
+
+// input.addEventListener("keyup", () => {
+//     console.log("keyup!!")
+// })//vice versa keydown
+
+
+//usefull property and method.
+input.addEventListener("keyup", e => {
+    // const assci = input.value
+    // console.log(assci)
+    // console.log(e.code)//which will give the specifc key name
+    // some other methods like --> e.ctrlKey,e.key,e.shiftKey
+})
