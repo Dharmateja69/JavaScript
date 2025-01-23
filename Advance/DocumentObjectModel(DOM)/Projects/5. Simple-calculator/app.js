3c54b63598526827abd68ee100a45c45220d09a3
Dharmateja69 <ddharmateja69@gmail.com>
2025-01-23 23:25:17 +0530

    project5 calculator

--- Advance/DocumentObjectModel(DOM)/Projects/5. Simple-calculator/app.js
+++ Advance/DocumentObjectModel(DOM)/Projects/5. Simple-calculator/app.js
@@ -1,21 +1,47 @@
+// document.getElementById("btn").addEventListener("click", () => {
+//   // Getting Elements From The DOM
+//   let num1 = document.querySelector(".num1").value;
+//   let num2 = document.querySelector(".num2").value;
+//   let result = document.querySelector(".result");
+//   let oprator = document.getElementById("selectOp").value;
+
+//   switch (oprator) {
+//     case "plus":
+//       result.innerHTML = +num1 + +num2;
+//       break;
+//     case "min":
+//       result.innerHTML = +num1 - +num2;
+//       break;
+//     case "dev":
+//       result.innerHTML = +num1 / +num2;
+//       break;
+//     case "multi":
+//       result.innerHTML = +num1 * +num2;
+//   }
+// });
+
 document.getElementById("btn").addEventListener("click", () => {
-  // Getting Elements From The DOM
+  //Get the elemens form the DOM
   let num1 = document.querySelector(".num1").value;
   let num2 = document.querySelector(".num2").value;
+  let operator = document.getElementById("selectOp");
+  const value = operator.value;
   let result = document.querySelector(".result");
-  let oprator = document.getElementById("selectOp").value;
-
-  switch (oprator) {
+  switch (value) {
     case "plus":
       result.innerHTML = +num1 + +num2;
       break;
+
     case "min":
       result.innerHTML = +num1 - +num2;
       break;
+
     case "dev":
       result.innerHTML = +num1 / +num2;
       break;
+
     case "multi":
       result.innerHTML = +num1 * +num2;
+      break;
   }
 });
