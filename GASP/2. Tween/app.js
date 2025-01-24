4e01c9747f02e8d987101a86238321429e4eae0b
Dharmateja69 <ddharmateja69@gmail.com>
2025-01-24 23:41:42 +0530

    tween gsap.to()

---
+++ GASP/2. Tween/app.js
@@ -0,0 +1,14 @@
+// gsap  this object is the enter point for the creation of tween ans controls, in generaly contians verious method.
+
+// what is a tween?
+
+// gsap.method(Element,vars obj).
+
+
+gsap.to(".box", {
+    x: 100,//transformX(100) we can use - also
+    duration: 2,
+    repeat: -1
+})
+
+// generalt to method is used in gasp , which is used to send the object to one place to another.
