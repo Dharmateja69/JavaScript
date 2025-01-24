0b7297114f91cbea867556f23de7e9e866e27ad0
Dharmateja69 <ddharmateja69@gmail.com>
2025-01-24 23:26:16 +0530

    jsAnimation

---
+++ GASP/1. JS Animation/app.js
@@ -0,0 +1,11 @@
+let sidebar = document.querySelector(".sidebar")
+let btn = document.querySelector("button")
+
+
+btn.addEventListener("click", () => {
+    sidebar.classList.toggle("hide")
+    // removing it if it's present and adding it if it's not present. If force is true, adds token (same as add()). If force is false, removes token (same as remove()).
+
+    // Returns true if token is now present, and false otherwise.
+
+})
