5e12a27e3e6af6ca60b6b4287fd007cec6f580d2
Dharmateja69 <ddharmateja69@gmail.com>
2025-02-01 16:38:49 +0530

    Constructor

---
+++ Oops/Constructor.js
@@ -0,0 +1,66 @@
+//// Constructor functions in JavaScript are regular functions
+// used with the new keyword to create and initialize objects
+// with shared properties and methods. They act as blueprints
+// for creating muttipte instances of objects with the same
+// structure and behavior.
+
+
+// function Createpeople(firstname, lastname, pl) {
+//     this.firstname = firstname
+//     this.lastname = lastname
+//     this.pl = pl
+//     this.info = function () {
+//         return `hello my name is ${firstname} and i love ${pl}`
+//     }
+
+// }
+
+// // NEW KEYWORD:
+// // 1. First create empty object {}
+// // 2. Sets "this" to point to that object
+// // 3. We can omit the return statement using "new" keyword
+
+// const jhon = new Createpeople("sidhu", "sidh", "javascript")
+// console.log(jhon)
+// console.log(jhon.info())
+
+// ===================================================================================
+
+// function Person(name, age, gender) { every constructor has a starting with capital letter
+//     this.name = name
+//     this.age = age
+//     this.gender = gender
+//     this.info = function () {
+//         return `my name is ${name} ,and age is ${age} and im ${gender}`
+//     }
+// }
+
+// const res1 = new Person("sidhu", 23, "male")
+// console.log(res1)
+// console.log(res1.info())
+// const res2 = new Person("sid", 34, "male")
+// console.log(res2)
+// console.log(res2.info())
+// ===========================================================================================
+
+// function Car(make, model, year, color) {
+//     this.make = make
+//     this.model = model
+//     this.year = year
+//     this.color = color
+//     this.start = function () {
+//         return `Starting the ${make} ${model}....`
+//     }
+//     this.stop = function () {
+//         return `Hwllo Stoppig the ${make} ${model}`
+//     }
+// }
+
+// const car1 = new Car("tata", "cvre", 2003, "red")
+// console.log(car1)
+// console.log(car1.start())
+// console.log(car1.stop())
+// const car2 = new Car("toyota", "cre", 2004, "blur")
+// console.log(car2)
+// console.log(car2.start())
+// console.log(car2.stop())
--- Oops/Factory.js
+++ Oops/Factory.js
@@ -6,3 +6,36 @@
 // }
 
 //  why we need to create a factory function rether then simple creating a obj.
+// function createPerson(firstname, lastname, pl) {
+//     return {
+//         firstname: firstname,
+//         lastname: lastname,
+//         pl: pl,
+//         fullame: function () {
+//             console.log(`Hello my name is ${this.firstname} ${this.lastname} and i love ${this.pl}`)
+//         }
+//     }
+// }
+
+// const jhon = createPerson("sidhu", "tejs", "sdijdhi")
+// console.log(jhon)
+// jhon.fullame()
+
+// =================================================================================================
+function vehiclestypes(type, brand, model, year) {
+    return {
+        // type: type,
+        // brand: brand,
+        // model: model,
+        // year: year,
+        //we can also write te above in the form of
+        type,
+        brand,
+        model,
+        year,
+
+    }
+}
+
+const jhon = vehiclestypes("rx100", "aoosdas", "32234asd", "2332")
+console.log(jhon)
